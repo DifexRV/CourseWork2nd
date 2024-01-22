2067a9484d77568fd2ec9d59e0aed2af1a7261f1
Roman <roOmkin@gmail.com>
2024-01-22 22:11:30 +0700

    Мастер ветка - Курсовая работа Java Core (2-го курса).

---
+++ src/main/java/com/example/coursework2nd/coursework2nd/CourseWork2ndApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework2nd.coursework2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2ndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursework2nd/coursework2nd/CourseWork2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2nd.coursework2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
